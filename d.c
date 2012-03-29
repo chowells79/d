50fc9ca2f444b50d96850ebb8f11279829ba3203
Carl Howells <chowells79@gmail.com>
2012-03-28 23:04:19 -0700

    I broked it

--- d.c
+++ d.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#include <sys/types.h>
-#include <sys/socket.h>
-
 #include <netinet/in.h>
-#include <arpa/inet.h>
 
 #include <mad.h>
 
+int create_listening_socket(const char *, in_port_t);
+
 int main() {
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-
-  struct in_addr addr;
-  addr.s_addr = inet_addr("127.0.0.1");
-
-  struct sockaddr_in saddr;
-  saddr.sin_family = AF_INET;
-  saddr.sin_port = 0x3333;
-  saddr.sin_addr = addr;
-
-  bind(sock, (struct sockaddr *) &saddr, sizeof(saddr));
-  listen(sock, 5);
+  int sock = create_listening_socket("127.0.0.1", 0x3333);
 
   struct sockaddr_in remote_addr;
   int size;
@@ -29,4 +16,21 @@ int main() {
   close(fd);
   shutdown(sock, SHUT_RDWR);
   return 0;
+}
+
+int create_listening_socket(const char *host_quad, in_port_t port) {
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct in_addr addr;
+  inet_aton(host_quad, &addr);
+
+  struct sockaddr_in saddr;
+  saddr.sin_family = AF_INET;
+  saddr.sin_port = htons(port);
+  saddr.sin_addr = addr;
+
+  bind(sock, (struct sockaddr *) &saddr, sizeof(saddr));
+  listen(sock, 5);
+
+  return sock;
 }
