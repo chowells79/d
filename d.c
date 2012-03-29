a724088a53b163927ff64ac80e10a0ea4fd023f9
Carl Howells <chowells79@gmail.com>
2012-03-28 18:08:27 -0700

    accepting an incoming connection is hard

--- d.c
+++ d.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#include <arpa/inet.h>
+
 #include "mad.h"
 
 int main() {
-  printf("%d\n", MAD_F_FRACBITS);
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct in_addr addr;
+  addr.s_addr = inet_addr("127.0.0.1");
+
+  struct sockaddr_in saddr;
+  saddr.sin_family = AF_INET;
+  saddr.sin_port = 0x3333;
+  saddr.sin_addr = addr;
+
+  bind(sock, (struct sockaddr *) &saddr, sizeof(saddr));
+  listen(sock, 5);
+
+  struct sockaddr_in remote_addr;
+  int size;
+  int fd = accept(sock, (struct sockaddr *) &remote_addr, &size);
   return 0;
 }
